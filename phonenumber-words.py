acb8e238675927ef76f13e23542da91c747d3e7e
Chinmay <cmburgul@gmail.com>
2020-05-09 13:49:17 -0400

    number_to_words

--- phonenumber-words.py
+++ phonenumber-words.py
@@ -29,8 +29,8 @@ def words_to_number(number):
 
     return op  
 
-# Find valid word
 def find_valid_word(exp, vocab):
+    # find valid word
     all_words = set()
     for word in vocab:
         hit = re.match(exp, word)
@@ -109,8 +109,65 @@ def all_wordification(number, vocab, min_len=1):
                     
     return ans
                     
+def number_to_words(number, vocab, min_len=1):
+    """ """
+    largest_word = 1
+    best_word = ""
+    ans = set()
+    num = number
+    number = number.replace("-","")
+    ans = ""
+    substrs = get_substrs(number, min_len)
+    
+    while (len(substrs)>0):
+        substr = substrs.pop()
+#         print("-----substr", substr, len(substr))
+        exp = "^" + "".join(["[" + "".join(mapping[char]) + "]" for char in substr]) + "$"
+        
+        word = find_valid_word(exp, vocab)
+        
+        if word == set():
+            word = None
+        else:
+            for w in word:
+                if (len(w) > largest_word):
+#                     print(w, substr)
+                    largest_word = len(w)
+                    
+                    first = number.find(substr)
+                    last = number.find(substr)+len(substr)-1
+    #                 print(first, last, len(w), w, number.replace(substr,w ))
+                    if (last==10 and first == 4 ):
+                        op = number.replace(substr,w)
+#                         print("#" +str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1]))
+                        ans = str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1])
+                    elif (last == len(number)-1 and first < 7):
+                        op = number.replace(substr,w)
+#                         print("*" + str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1]))
+                        ans = str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1])
+                    elif (last == len(number)-1 and first != 7):
+                        op = number.replace(substr,w)
+#                         print("!" + str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:7]) + "-"  + str(op[7:first]) + "-" + str(op[first:last+1]))
+                        ans = str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:7]) + "-"  + str(op[7:first]) + "-" + str(op[first:last+1])
+                    elif (last == len(number)-1 and first == 7):
+                        op = number.replace(substr,w)
+#                         print("!!" + str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1]))
+                        ans = str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1])
+                    elif (first == 4):
+                        op = number.replace(substr,w)
+#                         print("@" + str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:]))  
+                        ans =  str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:])
+                    elif (first <= 6):
+                        op = number.replace(substr,w)
+#                         print("+" + str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:]) )
+                        ans = str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:])
+                    else:
+                        op = number.replace(substr,w)
+#                         print("^" + str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:7]) + "-" + str(op[7:first]) +  str(op[first:last+1]) + "-" + str(op[last+1:]) )
+                        ans = str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:7]) + "-" + str(op[7:first]) +  str(op[first:last+1]) + "-" + str(op[last+1:])
+
+    return ans
             
 ph = "1-800-724-6837"
-ans = all_wordification(ph, vocab)
+ans = number_to_words(ph, vocab, 1)
 print(ans)
-
