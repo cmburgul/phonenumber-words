6c8efdca2978fc22f915cce32d7c96d57449860d
Chinmay <cmburgul@gmail.com>
2020-05-09 14:13:56 -0400

    Test cases

--- phonenumber-words.py
+++ phonenumber-words.py
@@ -1,5 +1,8 @@
 import re
 
+dictionary = "./scrabble/dictionary.txt"
+
+# mapping fot each number to possible characters 
 mapping = {
     '0':['0'],
     '1':['1'],
@@ -12,25 +15,28 @@ mapping = {
     '8':['T','U','V'],
     '9':['W','X','Y','Z']}
 
-dictionary = "./scrabble/dictionary.txt"
-
+# Loading vocabulary words from dictionary to vocab(dict)
 vocab = []
 with open(dictionary) as d:
     vocab = d.readlines()
 
-# words_to_number
+
 def words_to_number(number):
+    """ Converts words_to_number"""
+
+    number = "1-800-PAINTER"
+    number = number.replace("-","")
+    
     for ch in number:
 
         for key, val in mapping.items():
             if (ch in val):
                 number = number.replace(ch, key)
         op = str(number[0]) + "-" + str(number[1:4]) + "-" + str(number[4:7]) + "-" + str(number[7:])
-
     return op  
 
 def find_valid_word(exp, vocab):
-    # find valid word
+    """ finds valid word from a given set of characters matching in vocabulary"""
     all_words = set()
     for word in vocab:
         hit = re.match(exp, word)
@@ -39,7 +45,7 @@ def find_valid_word(exp, vocab):
     return all_words
 
 def get_substrs(string, min_substr_len = 1):
-    # substrs is a set to collect all substrings 
+    """ Gives all the possible substrings of a string""" 
     substrs = set()
 
     # Get all the substrings
@@ -55,14 +61,11 @@ def get_substrs(string, min_substr_len = 1):
     return substrs
 
 def all_wordification(number, vocab, min_len=1):
-    """ """
+    """ Gives all the possible wordification of a phone number """
     ans = set()
-    num = number
     number = number.replace("-","")
-    
     substrs = get_substrs(number)
-#     print(len(substrs))
-    
+
     while (len(substrs)>0):
         substr = substrs.pop()
         
@@ -73,55 +76,45 @@ def all_wordification(number, vocab, min_len=1):
         if word == set():
             word = None
         else:
-#             print(word)
+
             for w in word:
                 first = number.find(substr)
                 last = number.find(substr)+len(substr)-1
-#                 print(first, last, len(w), w, number.replace(substr,w ))
+
                 if (last==10 and first == 4 ):
                     op = number.replace(substr,w)
-#                     print("#" +str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1]))
                     ans.add(str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1]))
                 elif (last == len(number)-1 and first < 7):
                     op = number.replace(substr,w)
-#                     print("*" + str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1]))
                     ans.add(str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1]))
                 elif (last == len(number)-1 and first != 7):
                     op = number.replace(substr,w)
-#                     print("!" + str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:7]) + "-"  + str(op[7:first]) + "-" + str(op[first:last+1]))
                     ans.add(str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:7]) + "-"  + str(op[7:first]) + "-" + str(op[first:last+1]))
                 elif (last == len(number)-1 and first == 7):
                     op = number.replace(substr,w)
-#                     print("!!" + str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1]))
                     ans.add(str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1]))
                 elif (first == 4):
                     op = number.replace(substr,w)
-#                     print("@" + str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:]))  
                     ans.add(str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:]))
                 elif (first <= 6):
                     op = number.replace(substr,w)
-#                     print("+" + str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:]) )
                     ans.add(str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:]))
                 else:
                     op = number.replace(substr,w)
-#                     print("^" + str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:7]) + "-" + str(op[7:first]) +  str(op[first:last+1]) + "-" + str(op[last+1:]) )
                     ans.add(str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:7]) + "-" + str(op[7:first]) +  str(op[first:last+1]) + "-" + str(op[last+1:]))
                     
     return ans
                     
-def number_to_words(number, vocab, min_len=1):
-    """ """
+def number_to_word(number, vocab, min_len=1):
+    """ Gives the biggest possible wordification of a phone number"""
     largest_word = 1
-    best_word = ""
     ans = set()
-    num = number
     number = number.replace("-","")
     ans = ""
     substrs = get_substrs(number, min_len)
     
     while (len(substrs)>0):
         substr = substrs.pop()
-#         print("-----substr", substr, len(substr))
         exp = "^" + "".join(["[" + "".join(mapping[char]) + "]" for char in substr]) + "$"
         
         word = find_valid_word(exp, vocab)
@@ -131,43 +124,57 @@ def number_to_words(number, vocab, min_len=1):
         else:
             for w in word:
                 if (len(w) > largest_word):
-#                     print(w, substr)
                     largest_word = len(w)
                     
                     first = number.find(substr)
                     last = number.find(substr)+len(substr)-1
-    #                 print(first, last, len(w), w, number.replace(substr,w ))
                     if (last==10 and first == 4 ):
                         op = number.replace(substr,w)
-#                         print("#" +str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1]))
                         ans = str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1])
                     elif (last == len(number)-1 and first < 7):
                         op = number.replace(substr,w)
-#                         print("*" + str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1]))
                         ans = str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1])
                     elif (last == len(number)-1 and first != 7):
                         op = number.replace(substr,w)
-#                         print("!" + str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:7]) + "-"  + str(op[7:first]) + "-" + str(op[first:last+1]))
                         ans = str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:7]) + "-"  + str(op[7:first]) + "-" + str(op[first:last+1])
                     elif (last == len(number)-1 and first == 7):
                         op = number.replace(substr,w)
-#                         print("!!" + str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1]))
                         ans = str(op[0]) + "-" + str(op[1:4]) + "-" + str(op[4:first]) + "-" + str(op[first:last+1])
                     elif (first == 4):
                         op = number.replace(substr,w)
-#                         print("@" + str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:]))  
                         ans =  str(op[0]) + "-" + str(op[1:4]) + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:])
                     elif (first <= 6):
                         op = number.replace(substr,w)
-#                         print("+" + str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:]) )
                         ans = str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:first]) + "-" + str(op[first:last+1]) + "-" + str(op[last+1:])
                     else:
                         op = number.replace(substr,w)
-#                         print("^" + str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:7]) + "-" + str(op[7:first]) +  str(op[first:last+1]) + "-" + str(op[last+1:]) )
                         ans = str(op[0]) + "-" + str(op[1:4]) + "-"  + str(op[4:7]) + "-" + str(op[7:first]) +  str(op[first:last+1]) + "-" + str(op[last+1:])
 
     return ans
-            
-ph = "1-800-724-6837"
-ans = number_to_words(ph, vocab, 1)
-print(ans)
+
+
+if __name__ == '__main__':
+
+    # number_to_word
+    
+    ph = "1-800-724-6837"
+    ans = number_to_word(ph, vocab, 1)
+    print("Converting a number to wordified number ")
+    print("number : ", ph)
+    print("wordified number : ", ans)
+
+    print("\n----------")
+    # words_to_number
+    ph = "1-800-PAINTER"
+    ans = words_to_number(ph)
+    print("Converting a wordified number to number")
+    print("wordified number : ", ph)
+    print("numerified number : ", ans)
+
+    print("\n----------")
+    # number_to_words
+    ph = "1-800-724-6837"
+    ans = all_wordification(ph, vocab)
+    print("Converting a number to wordified numbers ")
+    print("number : ", ph)
+    print("wordified numbers : ", ans)
